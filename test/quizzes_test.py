c1305cf350e4970f30817083a0e11610cf9fb3f2
Maggie Trimpin <trimpinm@gmail.com>
2023-11-25 18:47:16 -0500

    All three tests

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -6,7 +6,7 @@ class QuizzesTest(unittest.TestCase):
 
     def setUp(self):
         # Run tests on non-production data
-        self.ctrl = QuizzesController('assignment.json')
+        self.ctrl = QuizzesController('quizzes_test.py')
     
     def test_expose_failure_01(self):
  
