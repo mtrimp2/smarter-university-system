6fdc1551a206e6721cce6e7b430ca152d07bc756
shwehtet <144739338+shwehtet@users.noreply.github.com>
2023-11-25 14:07:46 -0500

    Update quizzes_test.py
    This is the unit test 3.

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -1,29 +1,29 @@
 import unittest
-
+import datetime
 from app.controllers.quizzes_controller import QuizzesController
 
 class QuizzesTest(unittest.TestCase):
 
     def setUp(self):
         # Run tests on non-production data
-        self.ctrl = QuizzesController('quizzes_test.py')
-        
-    def test_expose_failure_01(self):
+        self.ctrl = QuizzesController('assignment.json')
+    
+    def test_expose_failure_03(self):
+ 
         """
-        Test retrieval of a quiz that does not exist 
-        This unit test will fail at file controller.py, line 117 
+        Fails to create a quiz, since the title parameter is None, expecting a string 
+        File "/home/arup/Documents/Enpm611/smarter-university-system-maggie_branch/app/controllers/quizzes_controller.py", line 63
         """
+
         self.ctrl.clear_data()
 
-        # Check that we have no quizzes in the list
-        quizzes = self.ctrl.get_quizzes()
-        self.assertEquals(len(quizzes), 0, "There are no quizzes in the list.")
-
-        # Try to retrieve quiz with incorrect id 
-        quiz = self.ctrl.get_quiz_by_id("random quiz id") #arbitrary string
-
-        self.assertIsNotNone(quiz, "The quiz can be retrieved.")
-        # This unit test should fail at file quizzes_controller.py, line 117 (?)
+        #Add a sample quiz to assignment.json files  
+        test_quiz_id = self.ctrl.add_quiz(None,"Math quiz 1",datetime.datetime(2020, 5, 17),datetime.datetime(2020, 5, 18))
+        # Try to retrieve recently incorrectly created quiz 
+        self.assertIsNotNone(test_quiz_id , "The test answer should not be retrievable")
+        self.ctrl.clear_data()
+if __name__ == '__main__':
+    unittest.main()
         
 
 if __name__ == '__main__':
