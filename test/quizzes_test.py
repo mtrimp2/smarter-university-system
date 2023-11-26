44698291a229c8645c47c7763fbe2f78852616b6
Joshua <63974346+joshuag1214@users.noreply.github.com>
2023-11-26 16:12:25 -0500

    Update quizzes_test.py
    updated unit test 1

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -6,35 +6,37 @@ class QuizzesTest(unittest.TestCase):
 
     def setUp(self):
         # Run tests on non-production data
-        self.ctrl = QuizzesController('quizzes_test.py')
+        self.ctrl = QuizzesController('assignment_test.json')
 
-    def test_expose_failure_01(self):#Maggie's code
- 
+    def test_expose_failure_01(self): # Maggie test
         """
-        Test use of integer 1 instead of boolean value in add_answer
+        Fails because None object is forced to be an iterable 
         """
+        #clears previous test 
         self.ctrl.clear_data()
-
-        #Check add files a sample quiz and receive its ID 
-        test_quiz_id = self.ctrl.add_quiz('quiz 1','True or false?',datetime(2020, 5, 18),datetime(2020, 5, 19))
+        #create quiz 1 and question 1 
+        quiz1 = self.ctrl.add_quiz("Quiz #1", "Biology", datetime.datetime(2020, 5, 17), datetime.datetime(2020, 5, 18))
+        self.ctrl.add_question(quiz1, "Question 1", "what is the power house of the cell")
         
-        # Add question
-        self.ctrl.add_question(test_quiz_id, "Question 1", "The sky is blue")
-        # quiz_id:str, title:str, text:str
-
-        # Add answer
-        self.ctrl.add_answer(test_quiz_id, "true", 1)
-        # question_id:str, text:str, is_correct:bool
-
-        """ 
-        File "/Users/maggietrimpin/Documents/repos/smarter-university-system/app/controllers/quizzes_controller.py", line 19, in __init__
-            self.quizzes:List[Quiz] = self._load_data()
-                              ^^^^^^^^^^^^^^^^^
-        File "/Users/maggietrimpin/Documents/repos/smarter-university-system/app/controllers/quizzes_controller.py", line 27, in _load_data
-            for qobj in load_data(self.file_name):
+        #attempt to create quiz 2 and question 1
+        quiz2 = self.ctrl.add_quiz("Quiz #2", "History ", datetime.datetime(2020, 5, 17), datetime.datetime(2020, 5, 18))
+        self.ctrl.quizzes[0].sections = None
+        quiz2question1 = self.ctrl.add_question(quiz2, "Question 1", "Where is the Roman empire located")
         """
-        self.assertIsNotNone(test_quiz_id, "The quiz can be retrieved.")
-        #self.ctrl.clear_data()    
+        File "/home/arup/Documents/Enpm611/smarter-university-system-main/app/controllers/quizzes_controller.py", line 81, in add_question
+            self._save_data()
+        File "/home/arup/Documents/Enpm611/smarter-university-system-main/app/controllers/quizzes_controller.py", line 54, in _save_data
+            json_data:any = [q.to_json() for q in self.quizzes]
+        File "/home/arup/Documents/Enpm611/smarter-university-system-main/app/controllers/quizzes_controller.py", line 54, in <listcomp>
+            json_data:any = [q.to_json() for q in self.quizzes]
+        File "/home/arup/Documents/Enpm611/smarter-university-system-main/app/model/assignments.py", line 117, in to_json
+            jobj['sections'] = [s.to_json() for s in self.sections]
+        TypeError: 'NoneType' object is not iterable
+
+        """
+        self.assertIsNotNone(quiz2question1,"Does not expect the 2nd quiz's question to be created")
+    
+        
 
     def test_expose_failure_02(self):#Joshua and Shwe's code
         """  
@@ -55,32 +57,31 @@ class QuizzesTest(unittest.TestCase):
         
         #Deletes contents in assignment.json
         self.ctrl.clear_data()
-
-    def test_expose_failure_03(self):#Harsh's unit test
-            self.ctrl.clear_data()
-
-            # add quiz 
-            quiz_id = self.ctrl.add_quiz("New quiz","New quiz",datetime.datetime.now(),datetime.datetime.now()+ datetime.timedelta(minutes=75))
-            # quiz added successfully
-            
-            # add question
-            question_id = self.ctrl.add_question(quiz_id,datetime.datetime.now(),"quiz question")
-            # Here it will cause an error as datetime object has been passed to function which requires a string. This will cause the function to crash
-
-            # Failing at 
-            """
-            File "d:\Fall 23\Software Engineering\smarter-university-system\app\controllers\quizzes_controller.py", line 81, in add_question
-            self._save_data()
-            ---
-            File "d:\Fall 23\Software Engineering\smarter-university-system\app\controllers\quizzes_controller.py", line 55, in _save_data
-            save_data(self.file_name,json_data)
-            """
-            # get question by id
-            question = self.ctrl.get_question_by_id(question_id)
-
-            self.assertIsNotNone(question,"Question")
-
     
 
-    if __name__ == '__main__':
-        unittest.main()
+    def test_expose_failure_03(self):#Harsh's unit test
+        self.ctrl.clear_data()
+
+        # add quiz 
+        quiz_id = self.ctrl.add_quiz("New quiz","New quiz",datetime.datetime.now(),datetime.datetime.now()+ datetime.timedelta(minutes=75))
+        # quiz added successfully
+        
+        # add question
+        question_id = self.ctrl.add_question(quiz_id,datetime.datetime.now(),"quiz question")
+        # Here it will cause an error as datetime object has been passed to function which requires a string. This will cause the function to crash
+
+        # Failing at 
+        """
+        File "d:\Fall 23\Software Engineering\smarter-university-system\app\controllers\quizzes_controller.py", line 81, in add_question
+        self._save_data()
+        ---
+        File "d:\Fall 23\Software Engineering\smarter-university-system\app\controllers\quizzes_controller.py", line 55, in _save_data
+        save_data(self.file_name,json_data)
+        """
+        # get question by id
+        question = self.ctrl.get_question_by_id(question_id)
+
+        self.assertIsNotNone(question,"Question")
+
+if __name__ == '__main__':
+    unittest.main()
