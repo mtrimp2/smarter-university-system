5c30c9d77a007f7e957791623c71fc6e4d093345
Joshua <63974346+joshuag1214@users.noreply.github.com>
2023-11-25 19:15:06 -0500

    Update quizzes_test.py
    Updated Unit test

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -6,10 +6,8 @@ class QuizzesTest(unittest.TestCase):
 
     def setUp(self):
         # Run tests on non-production data
-
         self.ctrl = QuizzesController('quizzes_test.py')
 
-    
     def test_expose_failure_01(self):#Maggie's code
  
         """
@@ -38,7 +36,6 @@ class QuizzesTest(unittest.TestCase):
         self.assertIsNotNone(test_quiz_id, "The quiz can be retrieved.")
         #self.ctrl.clear_data()    
 
-    
     def test_expose_failure_02(self):#Joshua and Shwe's code
         """  
         Fails to create a quiz, since the title parameter is None, expecting a string 
@@ -59,29 +56,31 @@ class QuizzesTest(unittest.TestCase):
         #Deletes contents in assignment.json
         self.ctrl.clear_data()
 
-def test_expose_failure_03(self):#Harsh's unit test
-        self.ctrl.clear_data()
+    def test_expose_failure_03(self):#Harsh's unit test
+            self.ctrl.clear_data()
 
-        # add quiz 
-        quiz_id = self.ctrl.add_quiz("New quiz","New quiz",datetime.datetime.now(),datetime.datetime.now()+ datetime.timedelta(minutes=75))
-        # quiz added successfully
-        
-        # add question
-        question_id = self.ctrl.add_question(quiz_id,datetime.datetime.now(),"quiz question")
-        # Here it will cause an error as datetime object has been passed to function which requires a string. This will cause the function to crash
+            # add quiz 
+            quiz_id = self.ctrl.add_quiz("New quiz","New quiz",datetime.datetime.now(),datetime.datetime.now()+ datetime.timedelta(minutes=75))
+            # quiz added successfully
+            
+            # add question
+            question_id = self.ctrl.add_question(quiz_id,datetime.datetime.now(),"quiz question")
+            # Here it will cause an error as datetime object has been passed to function which requires a string. This will cause the function to crash
 
-        # Failing at 
-        """
-        File "d:\Fall 23\Software Engineering\smarter-university-system\app\controllers\quizzes_controller.py", line 81, in add_question
-        self._save_data()
-        ---
-        File "d:\Fall 23\Software Engineering\smarter-university-system\app\controllers\quizzes_controller.py", line 55, in _save_data
-        save_data(self.file_name,json_data)
-        """
-        # get question by id
-        question = self.ctrl.get_question_by_id(question_id)
+            # Failing at 
+            """
+            File "d:\Fall 23\Software Engineering\smarter-university-system\app\controllers\quizzes_controller.py", line 81, in add_question
+            self._save_data()
+            ---
+            File "d:\Fall 23\Software Engineering\smarter-university-system\app\controllers\quizzes_controller.py", line 55, in _save_data
+            save_data(self.file_name,json_data)
+            """
+            # get question by id
+            question = self.ctrl.get_question_by_id(question_id)
 
-        self.assertIsNotNone(question,"Question")
-     
-if __name__ == '__main__':
-    unittest.main()
+            self.assertIsNotNone(question,"Question")
+
+    
+
+    if __name__ == '__main__':
+        unittest.main()
